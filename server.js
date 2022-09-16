966a1d09d439a647d0348c7d5e98a2b3f0526d14
Abhyudaya Mittal <abhyudaya2018cs170abesit@gmail.com>
2022-09-16 13:10:12 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const http = require("http");
+const cors = require("cors");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const socketServer = require("./socketServer");
+const authRoutes = require("./routes/authRoutes");
+const friendInvitationRoutes = require("./routes/friendInvitationRoutes");
+
+const PORT = process.env.PORT ||5000;
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+// register the routes
+app.use("/api/auth", authRoutes);
+app.use("/api/friend-invitation", friendInvitationRoutes);
+
+const server = http.createServer(app);
+socketServer.registerSocketServer(server);
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    server.listen(PORT, () => {
+      console.log(`Server is listening on ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("database connection failed. Server not started");
+    console.error(err);
+  });
+ 
+  if(process.env.NODE_ENV=="production"){
+    app.use(express.static("ui-code/build"));
+    const path=require("path");
+   app.get("*",(req,res)=>{
+     res.sendFile(path.resolve(__dirname,'ui-code','build','index.html'));
+    })
+  }
+  
+  
